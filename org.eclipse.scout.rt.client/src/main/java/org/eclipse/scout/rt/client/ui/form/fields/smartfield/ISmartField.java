808ac2ca5b17ad8b4becc2d9f20a4fe1f66f6278
David Wlazlo <david.wlazlo@bsi-software.com>
2025-08-29 15:58:50 +0200

    430951 Simplify process attribute management

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/smartfield/ISmartField.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/smartfield/ISmartField.java
@@ -29,6 +29,7 @@ import org.eclipse.scout.rt.shared.services.lookup.ILookupRowFetchedCallback;
 public interface ISmartField<VALUE> extends IValueField<VALUE> {
   int NOT_UNIQUE_ERROR_CODE = 1;
   int NO_RESULTS_ERROR_CODE = 2;
+  int NO_DATA_ERROR_CODE = 3;
 
   String PROP_DISPLAY_STYLE = "displayStyle";
   String PROP_RESULT = "result";
